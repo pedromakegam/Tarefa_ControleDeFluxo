21c8cc80678c8a133567fb392570fee5b96acb38
pedrovanderlansantana <pedromakegam@gmail.com>
2024-05-01 11:16:15 -0300

    dd

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+/**
+ * @author pedrovanderlansantana
+ * receber a nota de 4 alunos, dividir por 4 se for maior ou igual a 7 = aprovado, se maior ou igual a 5 de recuperacao se nao reprovado
+ *
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite a nota da primeira unidade: ");
+        double nota1 = teclado.nextDouble();
+        System.out.println("Digite a nota da segunda unidade: ");
+        double nota2 = teclado.nextDouble();
+        System.out.println("Digite a nota da terceira unidade: ");
+        double nota3 = teclado.nextDouble();
+        System.out.println("Digite a nota da quarta unidade: ");
+        double nota4 = teclado.nextDouble();
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+
+        if (media >= 7) {
+            System.out.println("Sua média foi: " + media + " - Aprovado!");
+        } else if (media >= 5) {
+            System.out.println("Sua média foi: " + media + " - Recuperação!");
+        } else {
+            System.out.println("Sua média foi: " + media + " - Reprovado!");
         }
+
     }
+
 }
+
